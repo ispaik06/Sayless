9b01fe486a42b1b177be9469bbfa41f74b7dbe71
Inseong <ispaik0602@gmail.com>
2026-06-25 20:35:35 +0900

    Initial Commit

---
+++ Sayless/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sayless
+//
+//  Created by IN-SEONG PAIK on 6/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sayless/SaylessApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaylessApp.swift
+//  Sayless
+//
+//  Created by IN-SEONG PAIK on 6/25/26.
+//
+
+import SwiftUI
+
+@main
+struct SaylessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
